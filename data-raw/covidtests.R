f1fdeca329f63a63955dfd278c7ba3e58b187cc6
Robert Link <rl8z@virginia.edu>
2020-06-12 08:43:39 -0400

    Impose a limit on how small neff can be.
    This prevents the perverse case where for N < cutoff Neff=N, but for N
    slightly above the cutoff, Neff could be much less than that.  It also
    fixes a problem where Neff was sometimes being adjusted to be less than
    the number of counties reporting cases.

--- data-raw/covidtests.R
+++ data-raw/covidtests.R
@@ -23,9 +23,11 @@ changed <- anti_join(vdhcovid::vadailytests, newdata, by=c('date', 'HealthDistri
 nchg <- nrow(changed)
 if(nchg > 0) {
   if(exists('ACCEPT_CHANGES') && ACCEPT_CHANGES) {
+    ACCEPT_CHANGES <- FALSE
     warning(nchg, ' changed entries found.  ACCEPT_CHANGES is set, so new entries will be adopted')
   }
   else {
+    changed <- left_join(changed, newdata, by=c('date','HealthDistrict'))
     stop(nchg, ' changed entries.  See table "changed" for list.')
   }
 }
@@ -47,6 +49,13 @@ vaweeklytests <- group_by(newdata, HealthDistrict, week) %>%
   ungroup() %>%
   arrange(date)
 
+## Minimum number of tests for adjustment.  If the nominal count was smaller than
+## this, then we won't adjust.  Also, we won't adjust to anything smaller than this
+## (The latter provision prevents the perverse situation where adding a few tests
+## takes you over the threshold and therefore gets you adjusted to something much
+## smaller)
+adjustmin <- 32
+
 ## The variance of a beta distribution is a*b/((a+b)^2(a+b+1)), where a = npos+1
 ## and b = nneg+1.  This is kind of beastly to solve for N, so we'll make the
 ## approximation that N is large enough that we can neglect the difference between
@@ -54,9 +63,13 @@ vaweeklytests <- group_by(newdata, HealthDistrict, week) %>%
 ## The Neff derived this way will not in general be an integer.  The neff, nposeff,
 ## are rounded to the nearest integer, and the fpos reported is nposeff/neff.
 vaweeklytests$neff <-
-  if_else(vaweeklytests$ntest > 32 & vaweeklytests$nday > 4 & vaweeklytests$npos > 0,
-          as.integer(round(vaweeklytests$fpos * (1-vaweeklytests$fpos) / vaweeklytests$varpos)),
-          vaweeklytests$ntest)
+    if_else(vaweeklytests$ntest > adjustmin & vaweeklytests$nday > 4 & vaweeklytests$npos > 0,
+              as.integer(
+                pmax(
+                  adjustmin,
+                  round(vaweeklytests$fpos * (1-vaweeklytests$fpos) / vaweeklytests$varpos)
+                )),
+                vaweeklytests$ntest)
 vaweeklytests$neff <- pmin(vaweeklytests$neff, vaweeklytests$ntest)          # Don't let neff be larger than ntest
 vaweeklytests$nposeff <- as.integer(round(vaweeklytests$neff * vaweeklytests$fpos))
 
